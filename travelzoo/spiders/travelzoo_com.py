4cea1516ccd9eeba78f28187217150f996f8815a
Claire Furney <clairefurney@gmail.com>
2015-02-14 11:54:25 +0000

    Remove .pyc from repo

--- travelzoo/spiders/travelzoo_com.py
+++ travelzoo/spiders/travelzoo_com.py
@@ -15,32 +15,58 @@ class TravelZooComSpider(CrawlSpider):
         start_urls.append('http://www.travelzoo.com/uk/uk-hotels-breaks/%s/' % category)
 
     rules = []
+    rules.append(Rule(LinkExtractor(deny=r'/Interstitial.aspx')))
+    rules.append(Rule(LinkExtractor(allow=r'/uk/entertainment/[\w-]+/\?tz_adid=\d+'), callback='parse_item', follow=True))
     for category in categories:
-        #rules.append(Rule(LinkExtractor(allow=r'/uk/uk-hotels-breaks/%s/' % category)))
         rules.append(Rule(LinkExtractor(allow=r'/uk/uk-hotels-breaks/%s/[\w-]+/' % category), callback='parse_item', follow=True))
 
     def parse_item(self, response):
         self.log("Parsing response")
-        i = TravelZooItem()
-        inner_deal_page = response.css('.innerDealPage')
-        i['name'] = inner_deal_page.css('h1').xpath('text()').extract()
-        i['url'] = response.url
-        i['description'] = inner_deal_page.css('.dealDetailsSection').css('.introDescription').xpath('node()').extract()
-        i['why_we_love_it'] = inner_deal_page.xpath(".//div[contains(@class,'dealDetailsSection') and h2[text()='Why we love it']]/ul").extract()
+        if len(response.css('.page.noBorder')):
+            return self.parse_item_no_border(response)
+        elif len(response.css('.page.withBorder')):
+            return self.parse_item_with_border(response)
+        else:
+            self.log("Parsing unknown page")
+            i = TravelZooItem()
+            i['name'] = response.css('h1::text').extract()
+            i['description'] = response.css('.dealText p:first-child').xpath('node()').extract()
+            i['whats_included'] = response.css('.dealText ul').extract()
+            return i
 
-        div_where = inner_deal_page.xpath(".//div[contains(@class,'dealDetailsSection') and h2[text()='Where']]")
+    def parse_item_with_border(self, response):
+        self.log("Parsing page with border")
+        i = TravelZooItem()
+        page = response.css('.innerDealPage')
+        self.log(page)
+        i['name'] = page.css('h1::text').extract()
+        i['description'] = page.css('.dealDetailsSection').css('.introDescription').xpath('node()').extract()
+        i['url'] = response.url
+        i['why_we_love_it'] = page.xpath(".//div[contains(@class,'dealDetailsSection') and h2[text()='Why we love it']]/ul").extract()
+
+        div_where = page.xpath(".//div[contains(@class,'dealDetailsSection') and h2[text()='Where']]")
         i['contact_name'] = div_where.xpath("div[contains(@id,'MerchantName')]/text()").extract()
         i['contact_address'] = div_where.xpath("div[contains(@id,'MerchantAddress')]/text()").extract()
         i['contact_map'] = div_where.xpath("div[contains(@id,'LinkMap')]/text()").extract()
         i['contact_phone'] = div_where.xpath("div[contains(@id,'MerchantPhone')]/text()").extract()
         i['contact_website'] = div_where.xpath("div[contains(@id,'MerchantWebsite')]/text()").extract()
 
-        i['whats_included'] = inner_deal_page.xpath(".//div[contains(@class,'dealDetailsSection') and h2[contains(text(), 'included')]]/ul").extract()
-        i['small_print'] = inner_deal_page.xpath(".//div[contains(@class,'dealDetailsSection') and h2[text()='The small print']]/p").xpath('node()').extract()
+        i['whats_included'] = page.xpath(".//div[contains(@class,'dealDetailsSection') and h2[contains(text(), 'included')]]/ul").extract()
+        i['small_print'] = page.xpath(".//div[contains(@class,'dealDetailsSection') and h2[text()='The small print']]/p").xpath('node()').extract()
 
         deal_page_right = response.css('#dealPageRightPart')
         i['price'] = deal_page_right.css('.buyNowBox .bigPrice').xpath('text()').extract()
         i['value'] = deal_page_right.css('.buyNowBox .value').xpath('text()').extract()
         i['discount'] = deal_page_right.css('.buyNowBox .discount').xpath('text()').extract()
         i['bought'] = deal_page_right.css('.buyNowBox .discountBox').xpath("span[contains(@id,'Bought')]/text()").extract()
+        return i
+
+    def parse_item_no_border(self, response):
+        self.log("Parsing page no border")
+        i = TravelZooItem()
+        page = response.css('.page.noBorder')
+        i['name'] = page.css('h1').xpath('text()').extract()
+        i['url'] = response.url
+        i['why_we_love_it'] = page.xpath(".//div[contains(@id,'spanWhyLove')]/ul").extract()
+        i['whats_included'] = page.xpath(".//div[contains(@id,'DivWhatsIncluded')]/div").xpath('node()').extract()
         return i
